3b03c5f1dc9cb44f4322cd57a47bdc33f4c7c41f
John <john.toriola.tj@gmail.com>
2022-08-14 22:57:49 +0100

    fixed select multi options for genres

--- app.py
+++ app.py
@@ -82,8 +82,6 @@ def index():
 
 @app.route('/venues')
 def venues():
-  # TODO: replace with real venues data.
-  #       num_upcoming_shows should be aggregated based on number of upcoming shows per venue.
   data = []
   unique_cities_and_state = set(db.session.query(Venue.city, Venue.state).all())
   for value in unique_cities_and_state:
@@ -109,7 +107,6 @@ def venues():
 
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for Hop should return "The Musical Hop".
   # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
   search_term = request.form['search_term'].lower().strip()
@@ -133,7 +130,6 @@ def search_venues():
 @app.route('/venues/<int:venue_id>')
 def show_venue(venue_id):
   # shows the venue page with the given venue_id
-  # TODO: replace with real venue data from the venues table, using venue_id
   venue = Venue.query.get(venue_id)
   all_past_shows = get_venue_past_shows(venue)
   all_upcoming_shows = get_venue_upcoming_shows(venue)
@@ -174,7 +170,7 @@ def create_venue_submission():
   state = request.form.get('state')
   address = request.form.get('address')
   phone = request.form.get('phone')
-  genres = request.form.get('genres')
+  genres = ",".join(request.form.getlist('genres'))
   facebook_link = request.form.get('facebook_link')
   image_link = request.form.get('image_link')
   website = request.form.get('website_link')
@@ -195,8 +191,6 @@ def create_venue_submission():
 
 @app.route('/venues/<venue_id>', methods=['DELETE'])
 def delete_venue(venue_id):
-  # TODO: Complete this endpoint for taking a venue_id, and using
-  # SQLAlchemy ORM to delete a record. Handle cases where the session commit could fail.
   error = False
   try:
     Show.query.filter_by(venue_id=venue_id).delete()
@@ -220,7 +214,6 @@ def delete_venue(venue_id):
 #  ----------------------------------------------------------------
 @app.route('/artists')
 def artists():
-  # TODO: replace with real data returned from querying the database
   data = []
   all_artists = Artist.query.all()
   for artist in all_artists:
@@ -233,7 +226,6 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
   # search for "band" should return "The Wild Sax Band".
   search_term = request.form['search_term'].lower().strip()
@@ -257,7 +249,6 @@ def search_artists():
 @app.route('/artists/<int:artist_id>')
 def show_artist(artist_id):
   # shows the artist page with the given artist_id
-  # TODO: replace with real artist data from the artist table, using artist_id
   artist = Artist.query.get(artist_id)
   all_past_shows = get_artist_past_shows(artist)
   all_upcoming_shows = get_artist_upcoming_shows(artist)
@@ -301,32 +292,17 @@ def edit_artist(artist_id):
     "seeking_description": selected_artist.seeking_description,
     "image_link": selected_artist.image_link
   }
-  # artist={
-  #   "id": 4,
-  #   "name": "Guns N Petals",
-  #   "genres": ["Rock n Roll"],
-  #   "city": "San Francisco",
-  #   "state": "CA",
-  #   "phone": "326-123-5000",
-  #   "website": "https://www.gunsnpetalsband.com",
-  #   "facebook_link": "https://www.facebook.com/GunsNPetals",
-  #   "seeking_venue": True,
-  #   "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
-  #   "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
-  # }
   
   # TODO: populate form with fields from artist with ID <artist_id>
   return render_template('forms/edit_artist.html', form=form, artist=artist)
 
 @app.route('/artists/<int:artist_id>/edit', methods=['POST'])
 def edit_artist_submission(artist_id):
-  # TODO: take values from the form submitted, and update existing
-  # artist record with ID <artist_id> using the new attributes
   name = request.form.get('name')
   city = request.form.get('city')
   state = request.form.get('state')
   phone = request.form.get('phone')
-  genres = request.form.get('genres')
+  genres = ','.join(request.form.getlist('genres'))
   facebook_link = request.form.get('facebook_link')
   image_link = request.form.get('image_link')
   website = request.form.get('website_link')
@@ -373,33 +349,16 @@ def edit_venue(venue_id):
     "seeking_description": selected_venue.seeking_description,
     "image_link": selected_venue.image_link
   }
-  # venue={
-  #   "id": 1,
-  #   "name": "The Musical Hop",
-  #   "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-  #   "address": "1015 Folsom Street",
-  #   "city": "San Francisco",
-  #   "state": "CA",
-  #   "phone": "123-123-1234",
-  #   "website": "https://www.themusicalhop.com",
-  #   "facebook_link": "https://www.facebook.com/TheMusicalHop",
-  #   "seeking_talent": True,
-  #   "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-  #   "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-  # }
-  # TODO: populate form with values from venue with ID <venue_id>
   return render_template('forms/edit_venue.html', form=form, venue=venue)
 
 @app.route('/venues/<int:venue_id>/edit', methods=['POST'])
 def edit_venue_submission(venue_id):
-  # TODO: take values from the form submitted, and update existing
-  # venue record with ID <venue_id> using the new attributes
   name = request.form.get('name')
   city = request.form.get('city')
   state = request.form.get('state')
   address = request.form.get('address')
   phone = request.form.get('phone')
-  genres = request.form.get('genres')
+  genres = ','.join(request.form.getlist('genres'))
   facebook_link = request.form.get('facebook_link')
   image_link = request.form.get('image_link')
   website = request.form.get('website_link')
@@ -443,8 +402,7 @@ def create_artist_submission():
   city = request.form.get('city')
   state = request.form.get('state')
   phone = request.form.get('phone')
-  genres = request.form.get('genres')
-  print("genres", genres)
+  genres = ','.join(request.form.getlist('genres'))
   facebook_link = request.form.get('facebook_link')
   image_link = request.form.get('image_link')
   website = request.form.get('website_link')
